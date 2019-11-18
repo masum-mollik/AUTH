89675dfdd9734e1e1ad487579598250139a6eef1
Masum Mollik <masum13@student.sust.edu>
2019-11-19 03:44:25 +0600

    Initial commit

---
+++ model/User.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+	//    name, email, birthday, phone , password
+	name: {
+		type: String,
+		required: true,
+		max: 255,
+		min: 6
+	},
+	email: {
+		type: String,
+		required: true,
+		max: 255,
+		min: 6
+	},
+	phone: {
+		type: String,
+		required: true,
+		min: 11
+	},
+	password: {
+		type: String,
+		required: true,
+		max: 2589,
+		min: 6
+	}
+});
+
+module.exports = mongoose.model("User", userSchema);
